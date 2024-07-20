8ab9d6c8c970125d29d1adbd12f7ede2b36b40e6
ElonTheKid <ilya.belous.2033@mail.ru>
2024-07-20 19:11:02 +0200

    full test wit problems

--- src/test/java/demo/qa/TextBoksFullfillFormTest.java
+++ src/test/java/demo/qa/TextBoksFullfillFormTest.java
@@ -22,22 +22,43 @@ public class TextBoksFullfillFormTest {
         $("[id=lastName]").setValue("Beluchi");
         $("#userEmail").setValue("examplemail@gmail.com");
         $$(".custom-control-label").get(1).shouldHave(text("Female")).click();
-        //$("#gender-radio-1").click();
-        $("#userNumber").setValue("888988889");
+        $("#userNumber").setValue("89024499555");
 
         // Найдите и кликните по полю ввода даты
         $("[id=dateOfBirthInput]").click();
         // Убедитесь, что календарь открылся
         $(".react-datepicker__month-container").shouldBe(visible);
         // Выберите месяц и год, если необходимо
-       $(".react-datepicker__month-select").selectOption("March"); // Замените на нужный месяц
-       $(".react-datepicker__year-select").selectOption("2002"); // Замените на нужный год
+        $(".react-datepicker__month-select").selectOption("March"); // Замените на нужный месяц
+        $(".react-datepicker__year-select").selectOption("2002"); // Замените на нужный год
         // Выберите день
         $$(".react-datepicker__day").findBy(text("19")).click();
         // Проверьте, что дата установлена правильно
-       $("[id=dateOfBirthInput]").shouldHave(value("19 Mar 2002"));
-       $("#uploadPicture").uploadFromClasspath("name.jpeg");
-        System.out.println();
+        $("[id=dateOfBirthInput]").shouldHave(value("19 Mar 2002"));
+        $("#uploadPicture").uploadFromClasspath("name.jpeg");
+
+        $$(".custom-control-label").get(3).shouldHave(text("Sports")).click();
+
+        //select the window "State"
+        $("[id=state]").click();
+        //Check that the window is open
+        $("#state").shouldBe(visible);
+        //Choose the certain State
+        $("#state").selectOption("NCR");
+        //$$("#state").findBy(text("NCR"));
+        $("#state").shouldHave(value("NCR"));
+
+        //select the window "City"
+        $("#city").click();
+        //Check that the window is open
+        $("#city").shouldBe(visible);
+        //Choose the certain City
+        $("#state").selectOption("Delhi");
+        $("#city").shouldHave(value("Delhi"));
+
+        $("#submit").click();
+        System.out.println("Test is done");
+        $("#submit").click();
 
 
 
